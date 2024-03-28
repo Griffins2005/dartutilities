6ceba2ab552443f16869625ec88fd286423fd294
Griffins2005 <134501193+Griffins2005@users.noreply.github.com>
2024-03-28 07:18:26 +0300

    initial commit

---
+++ switch.dart
@@ -0,0 +1,14 @@
+void main() {
+  String value = 'apple';
+
+  switch (value) {
+    case 'apple':
+      print('It is a fruit.');
+      break;
+    case 'car':
+      print('It is a vehicle.');
+      break;
+    default:
+      print('Unknown value');
+  }
+}
